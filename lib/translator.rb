13d79891cd28e79ec041ef821c20c3bce337fc39
Reed Roffis <reedroffis18@gmail.com>
2020-12-03 07:57:50 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,26 @@
 require 'yaml'
 
-def load_library
-  emoticons_array = YAML.load(File.read("lib/emoticons.yml"))
+def load_library(yml_file)
+  output_hash = {}
+  emoticons_hash = YAML.load(File.read(yml_file))
+  
+  # First element of emoticon array is the Western version and Second element of emoticon array is the Japanese version
+  emoticons_hash.each do |emoticon_name, emoticon_array|
+    each_emoticon_hash = {}
+    each_emoticon_hash[:english] = emoticon_array[0]
+    each_emoticon_hash[:japanese] = emoticon_array[1]
+    output_hash[emoticon_name] = each_emoticon_hash
+  end
+  output_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(yml_file, english_emoticon)
+  new_hash = load_library(yml_file)
+  japanese_output_emoticon = ''
+  new_hash.each do |emoticon_name, emoticon_hash|
+    
 end
 
-def get_english_meaning
+def get_english_meaning(yml_file, japanese_emoticon)
   # code goes here
 end
